d03e0bd5cd24d9882ce71a3fced00355dab6e3a7
Ramiz Raja <informramiz@gmail.com>
2020-04-27 16:59:49 +0500

    feat: Define default paint style

--- MinPaint/app/src/main/java/github/informramiz/minpaint/MiniPaintCanvasView.kt
+++ MinPaint/app/src/main/java/github/informramiz/minpaint/MiniPaintCanvasView.kt
@@ -3,6 +3,7 @@ package github.informramiz.minpaint
 import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.Canvas
+import android.graphics.Paint
 import android.view.View
 import androidx.annotation.ColorInt
 import androidx.core.content.ContextCompat
@@ -10,6 +11,9 @@ import androidx.core.content.ContextCompat
 /**
  * Created by Ramiz Raja on 27/04/2020
  */
+
+private const val STROKE_WIDTH = 12f
+
 class MiniPaintCanvasView(context: Context) : View(context) {
     //we will do our drawing on this bitmap and just supply it to system canvas when needed
     //so it basically serves a cache for us to make changes when next frame of this view is rendered
@@ -18,6 +22,26 @@ class MiniPaintCanvasView(context: Context) : View(context) {
     private lateinit var extraCanvas: Canvas
     @ColorInt
     private val backgroundColor = ContextCompat.getColor(context, R.color.colorBackground)
+    @ColorInt
+    private val drawColor = ContextCompat.getColor(context, R.color.colorPaint)
+    private val paint = Paint().apply {
+        //smooth out the edges of any shape drawn
+        isAntiAlias = true
+        //color to use for drawing with this paint
+        color = drawColor
+        //handle down-sampling of a color precision for devices not capable to handle
+        // high precision colors
+        isDither = true
+        //we want the style to be a line and don't want the shape to be filled so
+        //set it to stroke
+        style = Paint.Style.STROKE
+        //how to join line segments, we want it to be round
+        strokeJoin = Paint.Join.ROUND
+        //how the start and end of lines look
+        strokeCap = Paint.Cap.ROUND
+        //width of the stroke line
+        strokeWidth = STROKE_WIDTH
+    }
 
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
         super.onSizeChanged(w, h, oldw, oldh)
