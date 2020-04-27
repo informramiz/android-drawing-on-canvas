4f6f7929ebace79f1fe818cd0e9f293a64544c22
Ramiz Raja <informramiz@gmail.com>
2020-04-27 17:33:51 +0500

    feat: Handle user touch events

--- MinPaint/app/src/main/java/github/informramiz/minpaint/MiniPaintCanvasView.kt
+++ MinPaint/app/src/main/java/github/informramiz/minpaint/MiniPaintCanvasView.kt
@@ -4,9 +4,13 @@ import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.graphics.Paint
+import android.graphics.Path
+import android.view.MotionEvent
 import android.view.View
+import android.view.ViewConfiguration
 import androidx.annotation.ColorInt
 import androidx.core.content.ContextCompat
+import kotlin.math.abs
 
 /**
  * Created by Ramiz Raja on 27/04/2020
@@ -42,6 +46,19 @@ class MiniPaintCanvasView(context: Context) : View(context) {
         //width of the stroke line
         strokeWidth = STROKE_WIDTH
     }
+    //path contains the path of what is being drawn, we will use to hold
+    //the user touch path. Path in general encapsulates compound
+    // geometric paths consisting of straight line segments,
+    // quadratic curves, and cubic curves
+    private var path = Path()
+    //variables to store current position of the motion event
+    private var motionTouchEventX = 0f
+    private var motionTouchEventY = 0f
+    //variables to store start of a motion event
+    private var currentEventStartX = 0f
+    private var currentEventStartY = 0f
+    //define touch tolerance before the motion can be considered as drawing
+    private val touchTolerance = ViewConfiguration.get(context).scaledTouchSlop
 
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
         super.onSizeChanged(w, h, oldw, oldh)
@@ -61,5 +78,53 @@ class MiniPaintCanvasView(context: Context) : View(context) {
         //supply our cache bitmap to system canvas, set pain param to null, we will come back to it
         //later
         canvas.drawBitmap(canvasBitmap, 0f, 0f, null)
+    }
+
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        motionTouchEventX = event.x
+        motionTouchEventY = event.y
+
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> onTouchDown()
+            MotionEvent.ACTION_MOVE -> onTouchMove()
+            MotionEvent.ACTION_UP -> onTouchUp()
+        }
+
+        return true
+    }
+
+    private fun onTouchDown() {
+        //reset the path as this is start of the new drawing
+        path.reset()
+        //mark current event position as the start of the drawing
+        currentEventStartX = motionTouchEventX
+        currentEventStartY = motionTouchEventY
+        //move the path to the start point
+        path.moveTo(currentEventStartX, currentEventStartY)
+    }
+
+    private fun onTouchMove() {
+        //calculate the change in x and y coordinates
+        val dx = abs(motionTouchEventX - currentEventStartX)
+        val dy = abs(motionTouchEventY - currentEventStartY)
+        //check if it is a valid drawing command
+        if (dx >= touchTolerance && dy >= touchTolerance) {
+            //this is a valid drawing command so store it inside the path
+            //quadTo(...) draws more smooth line with rounded ends
+            path.quadTo(currentEventStartX, currentEventStartY,
+                (currentEventStartX + motionTouchEventX) / 2, (currentEventStartY + motionTouchEventY) / 2)
+            //draw the path to cache canvas which in turn will draw it on the cache bitmap
+            extraCanvas.drawPath(path, paint)
+            //now the command is drawn, store the new start for next command
+            currentEventStartX = motionTouchEventX
+            currentEventStartY = motionTouchEventY
+            //invalidate the view to request redraw()
+            invalidate()
+        }
+    }
+
+    private fun onTouchUp() {
+        //motion event has finished so let's reset the path
+        path.reset()
     }
 }
