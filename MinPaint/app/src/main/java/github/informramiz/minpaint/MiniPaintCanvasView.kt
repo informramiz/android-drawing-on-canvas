3154da90079ab9cbb0e8f3a258307d3834ee5fe4
Ramiz Raja <informramiz@gmail.com>
2020-04-27 16:49:58 +0500

    feat: Draw the cached bitmap to system canvas

--- MinPaint/app/src/main/java/github/informramiz/minpaint/MiniPaintCanvasView.kt
+++ MinPaint/app/src/main/java/github/informramiz/minpaint/MiniPaintCanvasView.kt
@@ -11,7 +11,10 @@ import androidx.core.content.ContextCompat
  * Created by Ramiz Raja on 27/04/2020
  */
 class MiniPaintCanvasView(context: Context) : View(context) {
+    //we will do our drawing on this bitmap and just supply it to system canvas when needed
+    //so it basically serves a cache for us to make changes when next frame of this view is rendered
     private lateinit var canvasBitmap: Bitmap
+    //canvas is used to draw over the bitmap so we need this for easy drawing
     private lateinit var extraCanvas: Canvas
     @ColorInt
     private val backgroundColor = ContextCompat.getColor(context, R.color.colorBackground)
@@ -27,5 +30,12 @@ class MiniPaintCanvasView(context: Context) : View(context) {
         canvasBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
         extraCanvas = Canvas(canvasBitmap)
         extraCanvas.drawColor(backgroundColor)
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        //supply our cache bitmap to system canvas, set pain param to null, we will come back to it
+        //later
+        canvas.drawBitmap(canvasBitmap, 0f, 0f, null)
     }
 }
