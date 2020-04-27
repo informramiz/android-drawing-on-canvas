178f21007cbf2b00767783bbd1bf7de6364c3e77
Ramiz Raja <informramiz@gmail.com>
2020-04-27 16:37:00 +0500

    feat: Write blank canvas view

--- MinPaint/app/src/main/java/github/informramiz/minpaint/MainActivity.kt
+++ MinPaint/app/src/main/java/github/informramiz/minpaint/MainActivity.kt
@@ -2,11 +2,16 @@ package github.informramiz.minpaint
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.Window
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(MiniPaintCanvasView(this).apply {
+            systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+            contentDescription = getString(R.string.canvasContentDescription)
+        })
     }
 }
---
+++ MinPaint/app/src/main/java/github/informramiz/minpaint/MiniPaintCanvasView.kt
@@ -0,0 +1,11 @@
+package github.informramiz.minpaint
+
+import android.content.Context
+import android.view.View
+
+/**
+ * Created by Ramiz Raja on 27/04/2020
+ */
+class MiniPaintCanvasView(context: Context) : View(context) {
+
+}
